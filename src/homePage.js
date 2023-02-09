af67225dbcdc0d146fce5c7fe967d978e3cccf97
matthew <permahuman@gmail.com>
2023-02-09 12:26:35 -0800

    Create media query to move nav to below header in case of less than 600px screen. Add delete button and edit button to individual projects div. Change for loop over projects array to insertAdjacentHTML with object informatio inserted per project and then inserted in to projects div class dynamically on homepagerender Module.

--- src/homePage.js
+++ src/homePage.js
@@ -47,27 +47,41 @@ const homePageString =
 content.insertAdjacentHTML('afterbegin', homePageString)
 
 
+
+
 for(let obj of projectStorage){
-    if( obj.project){
+    
 
-        let projectName =  document.createElement('h3')
-            projectName.textContent = obj.project
+      const  htmlIndividualProjectString = `
 
-        let projectDescription = document.createElement('p')
-            projectDescription.textContent = obj.description
+<div class = 'individualProjectContainer'>
 
-        let indiviualProjectContainer = document.createElement('div')
-            indiviualProjectContainer.classList.add('individualProjectContainer')
-            indiviualProjectContainer.append(projectName,projectDescription)
-      
-        let projectDiv = document.querySelector('.projectsDiv')
-      
-      
-        projectDiv.appendChild(indiviualProjectContainer)
+
+
+<div projectContainerButtonTitleContainer>
+
+<div class = 'projectContainerButtons'>
+<button > ... </button>
+<button> - </button>
+</div>
+
+<h3>${obj.project}</h3>
+
+
+</div>
+
+<p>${obj.description}</p>
+
+</div>
+
+
+
+`
+
     
     
-    }
-
+    const projectsDiv = document.querySelector('.projectsDiv')
+    projectsDiv.insertAdjacentHTML('afterbegin', htmlIndividualProjectString)
 }
 
 }
