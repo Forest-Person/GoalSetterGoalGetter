d8961980c1e66e0f2cd0f0038fe67c3372c545b9
matthew <permahuman@gmail.com>
2023-02-16 08:18:11 -0800

    Add if else to add todo event listener in renderaddtodoformpopup module that takes the form data and adds the todo to the local storage.

--- src/renderAddTodoFormPopup.js
+++ src/renderAddTodoFormPopup.js
@@ -1,10 +1,13 @@
 import { projectManager } from "./projectManager"
 import { homePageRender } from "./homePage"
+import { todoManager } from "./todoManager"
 
 
 
 const addTodoPopupForm = (projectName)=> {
 
+    const todo = todoManager()
+    
     const todoPopupFormContainer = document.querySelector(`.individualProjectContainer[data-project-name ='${projectName}']`)
 
     const addTodoFormHtmlString = `
@@ -19,23 +22,23 @@ const addTodoPopupForm = (projectName)=> {
                 
                     <form class = 'todoForm'>
                         <div>
-                            <label for = 'todoName'>To do:</label>
-                            <input type = 'text' name = 'todoName'>
+                            <label for = 'todoName'>Todo Name:</label>
+                            <input type = 'text' name = 'todoName' class = 'todoName'>
                         </div>
 
                         <div>
                             <label for = "todoDescription">Description:</label>
-                            <input type = "text" name = 'todoDescription'>
+                            <input type = "text" name = 'todoDescription' class = 'todoDescription'>
                         </div>
 
                         <div>
                             <label for = "dueDateOfTodo">Due Date:</label>
-                            <input type = "date" name = 'dueDateOfTodo'>
+                            <input type = "date" name = 'dueDateOfTodo' class = 'todoDueDate'>
                         </div>
 
                         <div>
                             <label for = "todoPriority">Priority:</label>
-                            <select name = "todoPriority">
+                            <select name = "todoPriority" class = 'todoPrioritySelection'>
                                 <option value = 'low'>Low</option>
                                 <option value = 'medium'>Medium</option>
                                 <option value = 'high'>High</option>
@@ -43,8 +46,8 @@ const addTodoPopupForm = (projectName)=> {
                         </div>
 
                         <div>
-                            <button type = 'button'>Add Todo</button>
-                            <button type = 'button'>Cancel</button>
+                            <button type = 'button' class = 'submitTodoButton'>Add Todo</button>
+                            <button type = 'button' class = 'cancelAddTodoButton'>Cancel</button>
                         </div>
 
                     </form>
@@ -54,6 +57,31 @@ const addTodoPopupForm = (projectName)=> {
     `
 todoPopupFormContainer.insertAdjacentHTML('afterbegin',addTodoFormHtmlString)
 
+
+const todoPopupFormContainerDiv = document.querySelector('.todoPopupFormContainer')
+
+todoPopupFormContainerDiv.addEventListener('click', (event)=> {
+
+    if(event.target.className === 'cancelAddTodoButton'){
+
+        todoPopupFormContainer.removeChild(todoPopupFormContainerDiv)
+    }
+
+    else if (event.target.className === 'submitTodoButton'){
+        //(projectName, nameOfTodo, descriptionOfTodo, dueDate, priority)
+        const todoName = document.querySelector('.todoName').value
+        const todoDescription = document.querySelector('.todoDescription').value
+        const todoDueDate = document.querySelector('.todoDueDate').value
+        const todoPriority = document.querySelector('.todoPrioritySelection').value
+
+        todo.addTodo(projectName,todoName,todoDescription,todoDueDate,todoPriority)
+        todoPopupFormContainer.removeChild(todoPopupFormContainerDiv)
+
+    }
+
+
+
+})
 
 
 }
